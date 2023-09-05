a40bf61d600b46caa7d24c490da23cb9c9431630
Taru Mahajan <arun-thakur@live.com>
2023-09-05 21:30:21 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddPlayersComponent } from './add-players/add-players.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPlayersComponent
   ],
   imports: [
     BrowserModule,
